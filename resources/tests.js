5f843375d27c3f2117ef219784fb50b4407d6dc3
EwenG <EwenG@users.noreply.github.com>
2016-03-16 00:47:01 +0100

    Update tests.js

--- resources/tests.js
+++ resources/tests.js
@@ -283,7 +283,7 @@ Suites.push({
                 keydownEvent.initEvent('keydown', true, true);
                 keydownEvent.keyCode = 13; // VK_ENTER
                 newTodo.dispatchEvent(keydownEvent);
-                newTodo = document.querySelector('.new-todo');
+                newTodo = contentDocument.querySelector('.new-todo');
                 newTodo.focus();
             }
         }),
